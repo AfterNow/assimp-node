d95a86bf20c773a221a830d6fd5612c378b6eccf
Jesse Vander Does <jesse@htmlfusion.com>
2017-10-17 21:53:14 -0700

    Added log to confirm if its loaded into cloud function

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { exec } = require('child_process');
 
 exports.assimp = function(args, cb) {
+		console.log("assimp node version 2");
 		let child = exec(`${__dirname}/debian/bin/assimp ${args}`, {env: {'LD_LIBRARY_PATH': `${__dirname}/debian/lib`}});
 		let result = "";
 		let error = "";
