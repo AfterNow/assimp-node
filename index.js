de6be59d664d65c3d1f3889b139f966bb8e64017
Jesse Vander Does <jesse@htmlfusion.com>
2017-11-13 17:50:51 -0800

    Added fbx 2 gltf

--- index.js
+++ index.js
@@ -19,3 +19,22 @@ exports.assimp = function(args, cb, options) {
 			cb()
 		});
 };
+
+exports.FBX2glTF = function(args, cb, options) {
+		let child = exec(`${__dirname}/debian/bin/FBX2glTF ${args}`, options);
+		let result = "";
+		let error = "";
+		child.stdout.on('data', (data) => {
+		    result += data;
+		});
+
+		child.stderr.on('data', (data) => {
+		    error += data;
+		});
+		
+		child.on('close', () => {
+			console.log(result);
+			console.error(error);
+			cb()
+		});
+};
